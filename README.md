26d7ab1612504994d0d6e3cf2e20ecd9e28e7596
jozsefDevs <jozsefDevs@users.noreply.github.com>
2014-07-19 01:58:05 -0700

    removed jshint from vim plugins plus improved readme

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ Based on dotfiles of Paul Irish. https://github.com/paulirish/dotfiles
 Make sure these hard dependencies are installed by default
 
 node: http://nodejs.org/
+1. Download latest node bins for linux 64 bit && extrat it to ~/installs/<nodever>
+2. `
+mkdir ~/bin && cd ~/bin
+ln -s ~/installs/<nodever>/bin/node node
+ln -s ~/installs/<nodever>/bin/npm npm
+`
+
 tmux: http://tmux.sourceforge.net/
 ag: (a.k.a. silver searcher): https://github.com/ggreer/the_silver_searcher
 editorconfig: http://editorconfig.org/
@@ -15,3 +22,4 @@ editorconfig: http://editorconfig.org/
 3. Set terminal background to black and font color to some grey
 4. Follow instructions in ~/.fonts/README.md for getting Powerline fonts
 5. Logout and log in!
+6. Go through all your vim plugins to check whether they are configured & installed properly.
--- install-deps.sh
+++ install-deps.sh
@@ -8,13 +8,14 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # VIM - install plugins
-cd ~/.vim/bundle && git clone https://github.com/walm/jshint.vim
 cd ~/.vim/bundle && git clone https://github.com/kien/ctrlp.vim
 cd ~/.vim/bundle && git clone https://github.com/editorconfig/editorconfig-vim
 cd ~/.vim/bundle && git clone https://github.com/mattn/emmet-vim
-cd ~/.vim/bundle && git clone https://github.com/walm/jshint.vim
 cd ~/.vim/bundle && git clone https://github.com/scrooloose/syntastic
+
 cd ~/.vim/bundle && git clone https://github.com/marijnh/tern_for_vim
+cd ~/.vim/bundle/tern_for_vim && npm install
+
 cd ~/.vim/bundle && git clone https://github.com/bling/vim-airline
 cd ~/.vim/bundle && git clone https://github.com/nathanaelkane/vim-indent-guides.git
 cd ~/.vim/bundle && git clone https://github.com/pangloss/vim-javascript.git
@@ -38,3 +39,6 @@ sudo easy_install Pygments
 # tmuxifier for smarter session handling
 git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
 
+# setting up environment for python
+sudo easy_install pip
+pip install virtualenv
