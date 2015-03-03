ba549bbdcf9d8cf18f22b3c647b09ca80574ac42
jozsefDevs <jozsefDevs@users.noreply.github.com>
2015-03-03 18:36:04 +0100

    renewed vim submodule install

--- install-deps.sh
+++ install-deps.sh
@@ -11,23 +11,7 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # VIM - install plugins
-cd ~/.vim/bundle && git clone https://github.com/kien/ctrlp.vim
-cd ~/.vim/bundle && git clone https://github.com/editorconfig/editorconfig-vim
-cd ~/.vim/bundle && git clone https://github.com/mattn/emmet-vim
-cd ~/.vim/bundle && git clone https://github.com/scrooloose/syntastic
-
-cd ~/.vim/bundle && git clone https://github.com/marijnh/tern_for_vim
-cd ~/.vim/bundle/tern_for_vim && npm install
-
-cd ~/.vim/bundle && git clone https://github.com/bling/vim-airline
-cd ~/.vim/bundle && git clone https://github.com/nathanaelkane/vim-indent-guides.git
-cd ~/.vim/bundle && git clone https://github.com/pangloss/vim-javascript.git
-cd ~/.vim/bundle && hg clone https://bitbucket.org/ludovicchabant/vim-lawrencium
-cd ~/.vim/bundle && git clone https://github.com/mhinz/vim-signify
-cd ~/.vim/bundle && git clone https://github.com/duganchen/vim-soy
-cd ~/.vim/bundle && git clone https://github.com/epmatsw/ag.vim
-cd ~/.vim/bundle && hg clone http://bitbucket.org/sjl/gundo.vim
-cd ~/.vim/bundle && git clone https://github.com/mhinz/vim-startify
+# see instructions in .vim/bundle/README
 
 # make default directory
 mkdir ~/code && cd ~/code
